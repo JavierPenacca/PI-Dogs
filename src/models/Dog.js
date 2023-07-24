932ffdd99eddc53801c4ce0e13d0c9ab12e616ef
JavierPenacca <javier.penacca@gmail.com>
2023-07-24 22:16:01 +0100

    Arreglos2

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -1,42 +1,56 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
+// const { v4: uuidv4 } = require('uuid');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  sequelize.define(
-    "dog",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        unique: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      image: {
-        type: DataTypes.STRING,
-        defaultValue: "No image available.",
-      },
-      height: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      weight: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lifeSpan: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      createdInDb: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-      },
+  // defino el modelo
+  sequelize.define('Dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
     },
-    {
-      timestamps: false,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [3, 20],
+          msg: 'Debe tener entre 3 y 20 caracteres'
+        }
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+		heightMin: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		heightMax: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		weightMin: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		weightMax: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+    createdBd: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     }
+  },
+    { freezeTableName: true, timestamps: false }
   );
 };
